f207b475b75d98fa8e8c886e656fa6f6394c01c3
Reb Sadran <reb@linux-98gx.site>
2014-12-26 13:48:36 +0200

    refactoring

---
+++ Plogbook/tests/tests.py
@@ -0,0 +1,24 @@
+import plogbook
+
+import unittest
+
+# Here's our "unit tests".
+class PlogTests(unittest.TestCase):
+
+    def test_truncate(self):
+        tests = ((['test', 20], 'test'),
+                 (['12345678910', 10], '1234567...'),
+                 (['12345678910', 10, False, True], '...5678910'),
+                 (['alapugaciova', 3, True], 'ala'),
+                 (['alapugaciova', 9, True, True], 'pugaciova'),
+                 (['banana', 5], 'ba...'),)
+
+        for test in tests:
+            self.failUnlessEqual(plogbook.Plog.truncate(*test[0]), test[1])
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
