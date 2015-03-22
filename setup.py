5c605537a939af6d15e0025885baae19d705db83
Bernard <bernardas@scrapinghub.com>
2015-03-22 16:21:34 +0200

    fixed not including default styles

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
@@ -9,6 +9,8 @@ setup(
     license='see LICENSE',
     author='granitas',
     author_email='bernardas.alisauskas@gmail.com',
+    package_data={'': ['/default_styles/*']},
+    include_package_data=True,
     description='Plogbook is a console application for personal logging. It turns personal logs written in console or '
                 'favorite text editor into readable html pages. The personal logs(plogs for short) are located in '
                 'categories which are part of the plogbook'
